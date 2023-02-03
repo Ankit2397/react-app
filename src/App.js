bc73a55dcb61f6a0e471518aca085232544e1cb3
Ankit2397 <ankit.singh@contriverz.com>
2023-02-03 18:09:09 +0530

    add day reminder app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import './App.css';
-import Line2 from "./Line2";
-//  Line 1 ****
- import data from "./data.json";
-import {
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-} from "recharts";
-import { Line } from "recharts";
- 
-
-export default function App() { 
-  return(     
-  <>               
-<Line1/>
-{/* <Line2/> */}
-</>
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   )
 }
 
-// Line 1 start 
-
- function Line1() { 
-  return(    
-    <div className="LineCharts">                
-<LineChart width={730} height={250} data={data}
-  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
-  <YAxis />
-  <Tooltip />
-  <Legend />
-  <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-  <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-  <Line type="monotone" dataKey="jws" stroke="#09CBCB" />
-  <Line type="monotone" dataKey="amt" stroke="#FF9757" />
-</LineChart>
-</div>
-  )
-}
-
-
-
-
-
-
-
+export default App
