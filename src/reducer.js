8371c7623e52b0b9d52e35e2a97a879f7905ea34
Ankit2397 <ankit.singh@contriverz.com>
2023-02-08 19:31:15 +0530

    add reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,36 +1,36 @@
 import { useReducer } from 'react';
 
-// function reducer(state, action) {
-//   if (action.type === 'increment') {
-//     return  state + 1
+function reducer(state, action) {
+  if (action.type === 'increment') {
+    return  state + 1
 
-//   } 
-//   if (action.type === 'decrement') {
-//     return  state - 1;
-//   }
-//   throw Error('Unknown action.');
-// }
+  } 
+  if (action.type === 'decrement') {
+    return  state - 1;
+  }
+  throw Error('Unknown action.');
+}
 
 // useReducer(reducer,initialstate)
 // ***** Switch case *****
 
-function reducer(state, action) {
-    switch (action.type) {
-      case 'increment': {
-        return {
-          name: state.name,
-          initialstate: state.initialstate + 1
-        };
-      }
-      case 'changed_name': {
-        return {
-          name: action.nextName,
-          initialstate: state.initialstate
-        };
-      }
-    }
-    throw Error('Unknown action: ' + action.type);
-  }
+// function reducer(state, action) {
+//     switch (action.type) {
+//       case 'increment': {
+//         return {
+//           name: state.name,
+//           initialstate: state.initialstate + 1
+//         };
+//       }
+//       case 'changed_name': {
+//         return {
+//           name: action.nextName,
+//           initialstate: state.initialstate
+//         };
+//       }
+//     }
+//     throw Error('Unknown action: ' + action.type);
+//   }
 const initialstate = 0;
 
 export default function Counter() {
