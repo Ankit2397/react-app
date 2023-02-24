d60c6e52593a58bee71c3564ecba3308dc8b8625
Ankit2397 <ankit.singh@contriverz.com>
2023-02-24 18:15:32 +0530

    ionic Ui

--- src/components/PageNotFound/PageNotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PageNotFound.scss";
-
-const PageNotFound = () => {
-  return <div className="not-found">PageNotFound</div>;
-};
-
-export default PageNotFound;
